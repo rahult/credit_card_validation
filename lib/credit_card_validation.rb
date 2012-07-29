800d6d00f97fb71af1367367615cdcf1052c0cd3
Rahul Trikha <rahul.trikha@gmail.com>
2012-07-29 23:32:09 +0930

    Added santization

--- lib/credit_card_validation.rb
+++ lib/credit_card_validation.rb
@@ -12,9 +12,18 @@ module CreditCardValidation
   class Validator
     attr_accessor :card
 
+    def initialize(card=nil)
+      sanitize(card)
+    end
+
+    def sanitize(card)
+      self.card = card.gsub(" ", "") unless self.card.nil?
+    end
+
     def type
+      sanitize(card)
       CARD_TYPES.each do |key, value|
-        return key if card =~ value
+        return key if sanitize(card) =~ value
       end
       "Unkown"
     end
@@ -22,5 +31,32 @@ module CreditCardValidation
     def valid?
       type != "Unkown"
     end
+
+    def validity
+      valid? ? "valid" : "invalid"
+    end
+
+    def to_s
+      "#{type}: #{card}".ljust(28) + " (#{validity})"
+    end
+  end
+end
+
+if __FILE__==$0
+  cards = "4111111111111111
+           4111111111111
+           4012888888881881
+           378282246310005
+           6011111111111117
+           5105105105105100
+           5105 1051 0510 5106
+           9111111111111111"
+
+  validator = CreditCardValidation::Validator.new
+
+  cards.lines.each do |line|
+    validator.card = line.chomp.strip
+    validator.valid?
+    puts validator
   end
 end
